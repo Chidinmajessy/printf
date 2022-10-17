41d7fb4e76f0f828b7921a9c0c07e404d540bbca
Chidinmajessy <111018400+Chidinmajessy@users.noreply.github.com>
2022-10-17 15:51:26 +0100

    team tasks

--- check_printf/converter.c
+++ check_printf/converter.c
@@ -6,7 +6,7 @@
  * @base: input base
  * @lowercase: flag if hexa values need to be lowercase
  * Return: result string
- * Authors: Ehoneah Obed & Abdulhakeem Badejo
+ * Authors: CHIDINMA EZE & Temiloluwa adewale
  */
 char *convert(unsigned long int num, int base, int lowercase)
 {
